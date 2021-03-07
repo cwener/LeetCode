6de6ef2a94b3aac7fbc94f906ae2384f94b641c1
chengwen02 <chengwen02@corp.netease.com>
2021-03-07 21:45:01 +0800

    反转链表

--- app/src/main/java/com/example/javatest/listnode/ReverseKList.java
+++ app/src/main/java/com/example/javatest/listnode/ReverseKList.java
@@ -62,4 +62,71 @@ class ReverseKList {
         // 从头节点往后移，一直移到m节点，将该节点视作头节点，则成了反转前 n-m+1 个节点
         return reverseBetween(head.next, m - 1, n - 1);
     }
+
+    /**
+     * 非递归反转
+     * @param head
+     * @return
+     */
+    public ListNode reverse(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        // 恰好在头节点反转时，头节点next指向pre,即null
+        ListNode pre = null;
+        ListNode next, cur = head;
+        while (cur != null) {
+            // 记住cur节点的next
+            next = cur.next;
+            // 拆开cur并反转
+            cur.next = pre;
+            // 向下一节点前进
+            pre = cur;
+            cur = next;
+        }
+        return pre;
+    }
+
+    /**
+     * 非递归反转a到b之间的节点
+     * @param a
+     * @param b
+     * @return
+     */
+    public ListNode reverseA2B(ListNode a, ListNode b) {
+        ListNode pre = null;
+        ListNode cur = a, next = a;
+        while (cur != b) {
+            next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+        return pre;
+    }
+
+    /**
+     * k个一组递归反转单链表
+     * @param head
+     * @param k
+     * @return
+     */
+    public ListNode reverseGroupK(ListNode head, int k) {
+        if (head == null) {
+            return null;
+        }
+        ListNode a = head, b = head;
+        for (int i = 0; i < k; i++) {
+            if (b == null) {
+                // 不满足k个则直接返回
+                return a;
+            }
+            b = b.next;
+        }
+        // 反转a b段
+        ListNode newHead = reverseA2B(a, b);
+        // 递归向后反转
+        a.next = reverseGroupK(b, k);
+        return newHead;
+    }
 }
